e38d7a5764946ed829e2358a3595b4f362f63780
Teddy Doll <tdoll.uwt@gmail.com>
2015-10-19 23:31:10 -0700

    feature: football widget - initial check in.

--- Football_Scores-master/app/src/main/java/barqsoft/footballscores/DatabaseContract.java
+++ Football_Scores-master/app/src/main/java/barqsoft/footballscores/DatabaseContract.java
@@ -44,6 +44,10 @@ public class DatabaseContract
         {
             return BASE_CONTENT_URI.buildUpon().appendPath("date").build();
         }
+        public static Uri buildScoreWithTwoDates()
+        {
+            return BASE_CONTENT_URI.buildUpon().appendPath("twoDates").build();
+        }
     }
     //URI data
     public static final String CONTENT_AUTHORITY = "barqsoft.footballscores";
