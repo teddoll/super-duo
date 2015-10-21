9b5cbba1d9092ecf8606c8e3166cd27ae82a04db
Teddy Doll <tdoll.uwt@gmail.com>
2015-10-20 23:59:34 -0700

    fixing rotation crash in Alex.

--- alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
+++ alexandria/app/src/main/java/it/jaschke/alexandria/BookDetail.java
@@ -66,6 +66,8 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
     }
 
 
+
+
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
         inflater.inflate(R.menu.book_detail, menu);
@@ -98,8 +100,9 @@ public class BookDetail extends Fragment implements LoaderManager.LoaderCallback
         Intent shareIntent = new Intent(Intent.ACTION_SEND);
         shareIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_DOCUMENT);
         shareIntent.setType("text/plain");
-        shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text)+bookTitle);
-        shareActionProvider.setShareIntent(shareIntent);
+        shareIntent.putExtra(Intent.EXTRA_TEXT, getString(R.string.share_text) + bookTitle);
+        if(shareActionProvider != null)
+            shareActionProvider.setShareIntent(shareIntent);
 
         String bookSubTitle = data.getString(data.getColumnIndex(AlexandriaContract.BookEntry.SUBTITLE));
         ((TextView) rootView.findViewById(R.id.fullBookSubTitle)).setText(bookSubTitle);
